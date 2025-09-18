cda93acc22c9152d7ea252dc3849a04ab0b31389
Juan Villa <ing.villa.jimenez@gmail.com>
2025-09-17 19:40:49 -0600

    feat: codigofacilito message

--- src/main/java/com/pubsub/subscriber/controller/PubSubController.java
+++ src/main/java/com/pubsub/subscriber/controller/PubSubController.java
@@ -42,7 +42,7 @@ public class PubSubController {
             return Mono.just(new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR));
         }
 
-        log.info("UserMessage info: {}", userMessage);
+        log.info("UserMessage info codigofacilito: {}", userMessage);
         return Mono.just(new ResponseEntity<>(userMessage.getBody(), HttpStatus.OK));
     }
 }
